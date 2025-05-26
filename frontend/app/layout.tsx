e7b2d0f92bf5cd04082df1b5118cf57fda977b91
rafatreggia <rafaeltreggia24@gmail.com>
2025-05-26 08:12:12 -0300

    2do comit para corregir funcionamiento

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import "./globals.css";
 import { Poppins, Pacifico } from "next/font/google";
 
@@ -19,16 +19,7 @@ export const pacifico = Pacifico({
   weight: "400",
 });
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "¡Genaro cumple 1 año!",
