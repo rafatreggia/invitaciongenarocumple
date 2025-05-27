4c3f11ce3871e6edef1036ab9c662e34fded0085
rafatreggia <rafaeltreggia24@gmail.com>
2025-05-27 07:03:46 -0300

    Refactor Hero component layout and add comments in Images component

--- frontend/components/landing/Hero.tsx
+++ frontend/components/landing/Hero.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className='myScreen min-h-[100vh] flex justify-center items-center'>Hero</div>
-  )
-}
+    <div className="myScreen min-h-[100vh] flex flex-col justify-center items-center">
+      <h2 className="">Bauti Cumple</h2>
+      <h1 className="">Genaro</h1>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- frontend/components/landing/Images.tsx
+++ frontend/components/landing/Images.tsx
@@ -7,3 +7,19 @@ const Images = () => {
 }
 
 export default Images
+
+
+// carousel para mostrar photos
+//https://21st.dev/cult-ui/3d-carousel/default   gusto mas esta
+//https://21st.dev/shadcnblockscom/gallery4/default
+
+
+//buttons
+//https://magicui.design/docs/components/shimmer-button
+//https://magicui.design/docs/components/pulsating-button
+
+
+//https://magicui.design/docs/components/confetti
+
+
+//https://magicui.design/docs/components/animated-circular-progress-bar
