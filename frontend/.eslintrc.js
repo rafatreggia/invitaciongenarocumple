cc1210ad1c990abb3b28ff862b03d241ea8d4cf7
rafatreggia <rafaeltreggia24@gmail.com>
2025-06-02 08:14:29 -0300

    añade regla para '@typescript-eslint/no-explicit-any' en .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,6 +2,8 @@
 module.exports = {
   extends: ['next/core-web-vitals', 'next/typescript'],
   rules: {
+     '@typescript-eslint/no-explicit-any':
+      process.env.NODE_ENV === 'production' ? 'off' : 'warn',
     '@typescript-eslint/no-unused-vars':
       process.env.NODE_ENV === 'production' ? 'off' : 'warn',
   },
